5792f402e150a05247ab33422f9492726c2fc8c6
jfegasu <jfegasu@gmail.com>
2025-02-22 20:25:41 -0500

    Create SPARK.mm

---
+++ SPARK.mm
@@ -0,0 +1,18 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1740273688335" ID="ID_1297228221" MODIFIED="1740273750931" TEXT="SPARK">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1740273818803" ID="ID_1876288434" MODIFIED="1740273846435" POSITION="right" STYLE="fork" TEXT="SparkSession">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1740273874314" ID="ID_897099306" MODIFIED="1740273877975" TEXT="builder">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1740273891187" ID="ID_910122563" MODIFIED="1740273894200" TEXT="master">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
